3b29b6897265f114448bf527d8eafe299a214996
thisoban <thisoban_1995@hotmail.com>
2022-04-19 15:38:53 +0200

    unittest created for two methods two succes and two failures

--- foodies-app-test/UnitTest1.cs
+++ foodies-app-test/UnitTest1.cs
@@ -1,6 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using foodies_app.Controllers;
+using foodies_app.Data;
+using foodies_app.Entities;
+using foodies_app.Interfaces;
 using NUnit.Framework;
 using Moq;
 using foodies_app.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace foodies_app_test;
 
@@ -9,12 +17,97 @@ public class MenuItemControllerTests
     [SetUp]
     public void Setup()
     {
+        
     }
 
     [Test]
-    public void Test1()
+    public void GetMenuItem_From_Id()
     {
-        var menuRepoMock = new Mock<IMenuRepository>();
+        var menuRepoMock = new Mock<IUnitOfWork>();
+        menuRepoMock.Setup(unitOfWork => unitOfWork.MenuRepository.GetMenuItem(1).Result).Returns(new MenuItem()
+        {
+            Id = 1,
+            Description = "salade",
+            Price = 10,
+            Title =" ceaser salade",
+            Category = null,
+            Allergy = null,
+        });
+        ;
+        var controller  = new MenuItemController(menuRepoMock.Object);
+        var result = controller.GetItem(1);
+        Assert.That(result.Result.Value.Title, Is.EqualTo("ceaser salade"));
         
     }
+
+    [Test]
+    public void GetMenuITem_From_id_failure()
+    {
+        var menuRepoMock = new Mock<IUnitOfWork>();
+        menuRepoMock.Setup(unitOfWork => unitOfWork.MenuRepository.GetMenuItem(1).Result).Returns(new MenuItem()
+        {
+            Id = 1,
+            Description = "salade",
+            Price = 10,
+            Title =" ceaser salade",
+            Category = null,
+            Allergy = null,
+        });
+        ;
+        var controller  = new MenuItemController(menuRepoMock.Object);
+        var result = controller.GetItem(2);
+        Assert.That(result.Result.Result, Is.InstanceOf(typeof(NotFoundObjectResult)));
+    }
+    [Test]
+    public void GetMenuItems_Equall_to_Three()
+    {
+        var menuRepoMock = new Mock<IUnitOfWork>();
+        menuRepoMock.Setup(unitOfWork => unitOfWork.MenuRepository.GetMenuItems().Result).Returns(new List<MenuItem>()
+        {
+            new MenuItem()
+            {
+                Id = 1,
+                Description = "salade",
+                Price = 10,
+                Title = " ceaser salade",
+                Category = null,
+                Allergy = null,
+            },
+            new MenuItem()
+            {
+                Id = 2,
+                Description = "broodje kip rijk gevuld aan salade en malse kip",
+                Price = 5,
+                Title = "broodje kip",
+                Category = null,
+                Allergy = null,
+            },
+            new MenuItem()
+            {
+            Id = 3,
+            Description = "lekkere gekoelde frits kola minder zoet maar even zo lekker",
+            Price = 3,
+            Title = " frits kola",
+            Category = null,
+            Allergy = null,
+        }
+        });
+        var controller  = new MenuItemController(menuRepoMock.Object);
+        var result = controller.GetMenu();
+       Assert.AreEqual(3,result.Result.Value.Count);
+    }
+
+    [Test]
+    public void GetMenuItems_Equall_to_Three_Failure()
+    {
+        var menuRepoMock = new Mock<IUnitOfWork>();
+        menuRepoMock.Setup(unitOfWork => unitOfWork.MenuRepository.GetMenuItems().Result).Returns(new List<MenuItem>()
+        {
+            
+        });
+        var controller  = new MenuItemController(menuRepoMock.Object);
+        var result = controller.GetMenu();
+        Assert.That(result.Result.Result, Is.Null);
+    }
+   
 }
