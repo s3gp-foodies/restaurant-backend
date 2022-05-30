8d2a4231bb5db0f8f8b003302b24531a43989b20
Bas <onrust.bas@gmail.com>
2022-05-30 14:10:07 +0200

    Added getOrder websocketcall

--- backend/DTOs/SubmittedProductDto.cs
+++ backend/DTOs/SubmittedProductDto.cs
@@ -2,6 +2,7 @@ namespace foodies_app.DTOs;
 
 public class SubmittedProductDto
 {
+    public int Id  { get; set; }
     public string Name { get; set; }
     public int Amount { get; set; }
     public string Category { get; set; }
